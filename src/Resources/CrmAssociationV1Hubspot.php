770a0fd9ab3e44b90cc24dce7f1b748890ca79e6
Luan <luan.tavares.lourenco@gmail.com>
2022-01-30 17:37:55 -0300

    Initial Commit 2

---
+++ src/Resources/CrmAssociationV1Hubspot.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace LTL\HubspotApi\Resources;
+
+use LTL\HubspotApi\Hubspot;
+
+/**
+* @method static $this get(int|string $objectId, int|string $definitionId) 
+* @method $this get(int|string $objectId, int|string $definitionId) 
+* @method static $this create(array $requestBody) 
+* @method $this create(array $requestBody) 
+* @method static $this delete() 
+* @method $this delete() 
+* @method static $this batchCreate(array $requestBody) 
+* @method $this batchCreate(array $requestBody) 
+* @method static $this batchDelete(array $requestBody) 
+* @method $this batchDelete(array $requestBody) 
+ */
+class CrmAssociationV1Hubspot extends Hubspot
+{
+    protected string $resource = "crm-associations-v1";
+}
---
+++ src/Resources/LineItemV1Hubspot.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LTL\HubspotApi\Resources;
+
+use LTL\HubspotApi\Hubspot;
+
+/**
+* @method static $this getAll() 
+* @method $this getAll() 
+* @method static $this get(int|string $id) 
+* @method $this get(int|string $id) 
+* @method static $this create(array $requestBody) 
+* @method $this create(array $requestBody) 
+* @method static $this update(int|string $id, array $requestBody) 
+* @method $this update(int|string $id, array $requestBody) 
+* @method static $this delete(int|string $id) 
+* @method $this delete(int|string $id) 
+* @method static $this getGroup(array $requestBody) 
+* @method $this getGroup(array $requestBody) 
+* @method static $this batchCreate(array $requestBody) 
+* @method $this batchCreate(array $requestBody) 
+* @method static $this batchUpdate(array $requestBody) 
+* @method $this batchUpdate(array $requestBody) 
+* @method static $this batchDelete(array $requestBody) 
+* @method $this batchDelete(array $requestBody) 
+ */
+class LineItemV1Hubspot extends Hubspot
+{
+    protected string $resource = "line-items-v1";
+}
