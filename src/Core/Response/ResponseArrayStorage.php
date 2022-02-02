21a045e5c784cb6859264476da3aa7b3b722494f
Luan <luan.tavares.lourenco@gmail.com>
2022-02-02 17:00:19 -0300

    Allow response array null

--- src/Core/Response/ResponseArrayStorage.php
+++ src/Core/Response/ResponseArrayStorage.php
@@ -1,23 +1,18 @@
 <?php
 namespace LTL\Hubspot\Core\Response;
 
-use LTL\Hubspot\Core\Exceptions\HubspotApiException;
 use LTL\Hubspot\Core\Response\Interfaces\ResponseInterface;
 
 abstract class ResponseArrayStorage
 {
     private static array $responses = [];
 
-    public static function get(ResponseInterface $response): array
+    public static function get(ResponseInterface $response): ?array
     {
         $hash = spl_object_hash($response);
 
         if (!array_key_exists($hash, self::$responses)) {
             self::$responses[$hash] = json_decode($response->get(), true);
-        }
-
-        if (is_null(self::$responses[$hash])) {
-            throw new HubspotApiException("Youn can't transform the response in array!\n". $response->get());
         }
 
         return self::$responses[$hash];
