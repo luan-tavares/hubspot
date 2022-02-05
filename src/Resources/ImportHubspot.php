ba4523f6289f49750e2029639d575d3e52da4345
Luan <luan.tavares.lourenco@gmail.com>
2022-02-05 13:27:13 -0300

    Add imports and site search

---
+++ src/Resources/ImportHubspot.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LTL\Hubspot\Resources;
+
+use LTL\Hubspot\Hubspot;
+
+/**
+ * @link https://app.hubspot.com/developer-docs/api?spec=v1/apis/crm/v3/imports
+ *
+ * @method static $this getAll() Returns a paged list of active imports for this account.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/crm/v3/imports
+ *
+ * @method $this getAll() Returns a paged list of active imports for this account.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/crm/v3/imports
+ *
+ * @method static $this get(int|string $importId) A complete summary of an import record, including any updates.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/crm/v3/imports
+ *
+ * @method $this get(int|string $importId) A complete summary of an import record, including any updates.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/crm/v3/imports
+ *
+ * @method static $this start(array $requestBody) Begins importing data from the specified file resources.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/crm/v3/imports
+ *
+ * @method $this start(array $requestBody) Begins importing data from the specified file resources.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/crm/v3/imports
+ *
+ * @method static $this cancel(int|string $importId, array $requestBody) This allows a developer to cancel an active import.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/crm/v3/imports
+ *
+ * @method $this cancel(int|string $importId, array $requestBody) This allows a developer to cancel an active import.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/crm/v3/imports
+ *
+ * @method static $this errors(int|string $importId) Get import error.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/crm/v3/imports
+ *
+ * @method $this errors(int|string $importId) Get import error.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/crm/v3/imports
+ *
+ */
+class ImportHubspot extends Hubspot
+{
+    protected string $resource = "imports-v3";
+}
---
+++ src/Resources/SiteSearchHubspot.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LTL\Hubspot\Resources;
+
+use LTL\Hubspot\Hubspot;
+
+/**
+ * @link https://app.hubspot.com/developer-docs/api?spec=v1/apis/cms/v3/site-search
+ *
+ * @method static $this getIndexes(int|string $contentId) For a given account and document ID (page ID, blog post ID, HubDB row ID, etc.), return all indexed data for that document.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/cms/v3/site-search
+ *
+ * @method $this getIndexes(int|string $contentId) For a given account and document ID (page ID, blog post ID, HubDB row ID, etc.), return all indexed data for that document.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/cms/v3/site-search
+ *
+ * @method static $this search() Returns any website content matching the given search criteria for a given HubSpot account.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/cms/v3/site-search
+ *
+ * @method $this search() Returns any website content matching the given search criteria for a given HubSpot account.
+ * See https://app.hubspot.com/developer-docs/api?spec=v1/apis/cms/v3/site-search
+ *
+ */
+class SiteSearchHubspot extends Hubspot
+{
+    protected string $resource = "site-search-v3";
+}
