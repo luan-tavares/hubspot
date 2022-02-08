8eead0c7bd95280ade1b0ccb1790b0d0477e8c4a
Luan <luan.tavares.lourenco@gmail.com>
2022-02-08 18:13:39 -0300

    v1.0.34

--- examples/associations-v4.php
+++ examples/associations-v4.php
@@ -5,21 +5,21 @@ require_once __DIR__ .'/__init.php';
 
 use LTL\Hubspot\Resources\AssociationHubspot;
 
-// dd(
-//     AssociationHubspot::deleteDefinition(
-//         '2-4031056',
-//         'deals',
-//         181
-//     ),
-//     AssociationHubspot::updateDefinition(
-//         'deals',
-//         '2-4031056',
-//         [
-//             'label' => 'teste_adm_to_deal',
-//             'associationTypeId' => 158
-//         ]
-//     )
-// );
+dd(
+    AssociationHubspot::deleteDefinition(
+        '2-4031056',
+        'deals',
+        181
+    ),
+    AssociationHubspot::updateDefinition(
+        'deals',
+        '2-4031056',
+        [
+            'label' => 'teste_adm_to_deal',
+            'associationTypeId' => 158
+        ]
+    )
+);
 
 // dump(AssociationHubspot::getDefinition('contacts', 'deals'));
 dump(AssociationHubspot::createDefinition('d', 'dsd', ['s' => 5]));
