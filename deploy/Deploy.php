210063119619629f7ce09402a33a806b27390ac1
Luan <luan.tavares.lourenco@gmail.com>
2022-02-08 15:54:47 -0300

    v1.0.37

--- deploy/Deploy.php
+++ deploy/Deploy.php
@@ -28,6 +28,8 @@ class Deploy
         $diff = shell_exec('git diff');
 
         if ($diff == '') {
+            print("\033[1;35mNothing to commit\033[0m".PHP_EOL);
+
             die();
         }
 
