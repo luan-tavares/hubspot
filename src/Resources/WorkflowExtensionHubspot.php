b8a338660ec7deb16450d366e7fb8eb7ff200986
Luan <luan.tavares.lourenco@gmail.com>
2022-02-10 23:43:30 -0300

    v1.0.35

--- src/Resources/WorkflowExtensionHubspot.php
+++ src/Resources/WorkflowExtensionHubspot.php
@@ -7,16 +7,28 @@ use LTL\Hubspot\Hubspot;
 /**
  * @link https://developers.hubspot.com/docs/api/automation/custom-workflow-actions
  *
- * @method static $this completeCallback(int|string $callbackId, array $requestBody) Completes the given action callback.
+ * @method static $this completeCallback(int|string $callbackId, array $requestBody) Completes the given action callback using app oAuth.
  * See https://developers.hubspot.com/docs/api/automation/custom-workflow-actions
  *
- * @method $this completeCallback(int|string $callbackId, array $requestBody) Completes the given action callback.
+ * @method $this completeCallback(int|string $callbackId, array $requestBody) Completes the given action callback using app oAuth.
  * See https://developers.hubspot.com/docs/api/automation/custom-workflow-actions
  *
- * @method static $this batchCompleteCallbacks(array $requestBody) Completes the given action callbacks.
+ * @method static $this batchCompleteCallbacks(array $requestBody) Completes the given action callbacks using app oAuth.
  * See https://developers.hubspot.com/docs/api/automation/custom-workflow-actions
  *
- * @method $this batchCompleteCallbacks(array $requestBody) Completes the given action callbacks.
+ * @method $this batchCompleteCallbacks(array $requestBody) Completes the given action callbacks using app oAuth.
+ * See https://developers.hubspot.com/docs/api/automation/custom-workflow-actions
+ *
+ * @method static $this completeCallbackApp(int|string $appId, int|string $callbackId, array $requestBody) Completes the given action callback using account apikey.
+ * See https://developers.hubspot.com/docs/api/automation/custom-workflow-actions
+ *
+ * @method $this completeCallbackApp(int|string $appId, int|string $callbackId, array $requestBody) Completes the given action callback using account apikey.
+ * See https://developers.hubspot.com/docs/api/automation/custom-workflow-actions
+ *
+ * @method static $this batchCompleteCallbacksApp(int|string $appId, array $requestBody) Completes the given action callbacks using account apikey.
+ * See https://developers.hubspot.com/docs/api/automation/custom-workflow-actions
+ *
+ * @method $this batchCompleteCallbacksApp(int|string $appId, array $requestBody) Completes the given action callbacks using account apikey.
  * See https://developers.hubspot.com/docs/api/automation/custom-workflow-actions
  *
  */
