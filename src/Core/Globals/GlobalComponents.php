b269bdc1d16b08926bcc455c78ed87c53b86f6d4
Luan Lourenço <luan.tavares.lourenco@gmail.com>
2026-08-02 18:22:59 -0300

    v3.6.0.000

--- src/Core/Globals/GlobalComponents.php
+++ src/Core/Globals/GlobalComponents.php
@@ -18,6 +18,6 @@ abstract class GlobalComponents implements GlobalComponentInterface, StaticPubli
     public static function setGlobalOAuth(string $token): void
     {
         OAuthGlobal::store($token);
+        /** */
     }
-
 }
